77430f19a542c097eb780bcc4e8ebf10007b186f
KaBence <kabaly.bence@gmail.com>
2023-05-08 11:32:30 +0200

    table room is created

--- Sep2 - Database/temp.sql
+++ Sep2 - Database/temp.sql
@@ -5,3 +5,16 @@ set schema 'hostelreservation';
 create table temp(
     test varchar(50)
 );
+
+create table Room(
+    roomNo int,
+    noBeds int,
+    size int,
+    orientation varchar(20),
+    internet boolean,
+    bathroom boolean,
+    kitchen boolean,
+    balcony boolean,
+    price int
+);
+
