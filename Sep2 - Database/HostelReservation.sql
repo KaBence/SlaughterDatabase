976f8e5a4e486e7382a85f59a6a290a9854d3642
KaBence <kabaly.bence@gmail.com>
2023-05-16 15:43:36 +0200

    little changes constraints stb.....

--- Sep2 - Database/HostelReservation.sql
+++ Sep2 - Database/HostelReservation.sql
@@ -4,6 +4,36 @@ create schema if not exists hostelReservation;
 
 set schema 'hostelreservation';
 
+create domain stat as varchar(50) check ( value in ('Free','Reserved','Booked','Reserved and Booked' ));
+
+create or replace function status()
+    returns TRIGGER
+    language plpgsql
+as
+    $$
+    declare
+        roomNumber int;
+        reserved int;
+        booked int;
+        begin
+            roomNumber=new.roomno;
+            select count(*) into reserved from reservedby where roomNo=roomNumber and checkedIn=false;
+            select count(*) into booked from reservedby where roomNo=roomNumber and checkedIn=true;
+            if reserved > 0 then
+                update room set status='Reserved' where roomNo=roomNumber;
+            end if;
+            if booked>0 then
+                update room set status='Booked' where roomNo=roomNumber;
+            end if;
+            if booked>0 and reserved>0 then
+                update room set status='Reserved and Booked' where roomNo=roomNumber;
+            end if;
+            return new;
+        end;
+    $$;
+
+
+
 create table Room(
     roomNo int primary key ,
     noBeds int,
@@ -13,14 +43,15 @@ create table Room(
     bathroom boolean,
     kitchen boolean,
     balcony boolean,
-    price int
+    price int,
+    status stat
 );
 
-insert into room (roomNo, noBeds, size, orientation, internet, bathroom, kitchen, balcony, price)
-values (10,2,25,'West',true,true,false,false,300);
+insert into room (roomNo, noBeds, size, orientation, internet, bathroom, kitchen, balcony, price,status)
+values (10,2,25,'West',true,true,false,false,300,'Free');
 
-insert into room (roomNo, noBeds, size, orientation, internet, bathroom, kitchen, balcony, price)
-values (20,1,18,'East',true,false,true,true,270);
+insert into room (roomNo, noBeds, size, orientation, internet, bathroom, kitchen, balcony, price,status)
+values (20,1,18,'East',true,false,true,true,270,'Free');
 
 create table "user"(
     username varchar(100) primary key,
@@ -51,12 +82,18 @@ create table ReservedBy(
     roomNo int,
     username varchar(100),
     fromDate DATE,
-    toDate DATE,
+    toDate DATE check ( ReservedBy.toDate >ReservedBy.fromDate),
     checkedIn boolean,
     PRIMARY KEY(roomNo,username,fromDate),
     FOREIGN KEY (roomNo) references Room(roomno),
     FOREIGN KEY (username) references "user"(username)
 );
+
+create or replace trigger changeStatus
+    after insert
+    on reservedby
+    for each row
+    execute function status();
 
 INSERT INTO ReservedBy(roomNo, username, fromDate, toDate, checkedIn)
 VALUES(10, 'john@hotmail.com','2023/4/10', '2023/4/15', true);
