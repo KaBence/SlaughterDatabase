dc6cbb343d631b4b6bec79f84911e435d8eaff72
Michaela202 <veselovskamichaela222@gmail.com>
2023-11-08 20:26:20 +0100

    database version 2

--- SlaughterDatabase/tables.sql
+++ SlaughterDatabase/tables.sql
@@ -2,7 +2,7 @@ drop schema if exists SlaughterHouse cascade ;
 
 create schema SlaughterHouse;
 
-set schema 'SlaughterHouse';
+set schema 'slaughterhouse';
 
 create table Animal(
     anm_id serial primary key ,
@@ -23,7 +23,9 @@ create table AnimalPart(
     anm_pt_name varchar(100),
     weight double precision,
     anm_id int,
-    FOREIGN KEY (anm_id) references Animal(anm_id)
+    tray_id int,
+    FOREIGN KEY (anm_id) references Animal(anm_id),
+    FOREIGN KEY (tray_id) references Tray(tray_id)
 
 );
 
@@ -38,26 +40,21 @@ VALUES ('leg',5,1);
 
 create table Tray(
     tray_id serial primary key ,
-    anm_pt_name varchar(100),
     maxWeight double precision,
-    p_id int,
-    FOREIGN KEY (p_id) references AnimalPart(p_id),
-    FOREIGN KEY (anm_pt_name) references AnimalPart(anm_pt_name)
+    OneKindPackege_id int,
+    HalfAnAnimalPackage_id int,
+    FOREIGN KEY (OneKindPackege_id) references OneKindPackage(package_id),
+    FOREIGN KEY (HalfAnAnimalPackage_id) references HalfAnAnimalPackage(package_id)
 );
 
-insert into Tray( anm_pt_name,tray_id,maxWeight, p_id)
-VALUES ('head',1,25,2);
+insert into Tray(tray_id,maxWeight,OneKindPackege_id, HalfAnAnimalPackage_id)
+VALUES (1,25,1,null);
 
-insert into Tray( anm_pt_name,tray_id,maxWeight, p_id)
-VALUES ('leg',1,25,1);
- insert into Tray(anm_pt_name,tray_id, maxWeight, p_id) VALUES ('leg',2,20,3);
 
 create table OneKindPackage(
     partType varchar(100),
-    package_id serial primary key ,
-    tray_id int,
-    FOREIGN KEY (tray_id) references Tray(tray_id),
-    FOREIGN KEY (partType) references Tray(anm_pt_name)
+    package_id serial primary key
+
 );
 
 insert into OneKindPackage(partType
@@ -66,7 +63,5 @@ insert into OneKindPackage(partType)
 VALUES ('head');
 
 create table HalfAnAnimalPackage(
-    package_id serial primary key ,
-    tray_id int,
-    FOREIGN KEY (tray_id) references Tray(tray_id)
+    package_id serial primary key
 );
